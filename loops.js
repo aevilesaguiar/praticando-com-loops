bb4435265a517634476cdd08db30733dd0623b6c
Aeviles Aguiar Silva <52088444+aevilesaguiar@users.noreply.github.com>
2021-08-30 11:58:57 -0300

    first commit

---
+++ loops.js
@@ -0,0 +1,15 @@
+for (let i= 0/*contador */;i<=4/*contador */; i++/*incremento*/ ){
+
+    console.log(i + " oi");
+
+}
+
+for (let i= 0/*contador */;i<=4/*contador */; i++/*incremento*/ ){
+
+    console.log("Olá, mundo");
+
+}
+
+for(let volta = 1; volta<=5; volta++){
+    console.log(`Volta número: ${volta}`);
+}
